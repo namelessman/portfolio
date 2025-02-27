670bc4ec7821789159106536f616d9e1a6eeb3e5
namelessman <zhoumq.good@gmail.com>
2025-02-28 00:18:14 +1300

    chore: add GA and SEO keywords

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -15,6 +16,13 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Namelessman's Portfolio",
   description: "Code simplifies the world, liberates humanity — I create it.",
+  keywords: [
+    "namelessman",
+    "portfolio",
+    "front end",
+    "full stack",
+    "developer",
+  ],
 };
 
 export default function RootLayout({
@@ -29,6 +37,7 @@ export default function RootLayout({
       >
         {children}
       </body>
+      <GoogleAnalytics gaId="G-KSC1NTLKDV" />
     </html>
   );
 }
